3664d42bde732603b2300ce563f0a911faa22bcb
Klogo Gibson Selasi <gibson.klogo@paddle.com>
2023-11-07 13:31:56 +0000

    revert to existing hash router

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import About from 'screens/About/About'
 import Home from '../screens/Home/Home'
 import Jigsaw from 'screens/Jigsaw/jigsaw'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/jigsaw',
         element: <Jigsaw />,
